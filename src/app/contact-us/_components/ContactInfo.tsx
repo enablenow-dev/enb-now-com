e1997359e360d55c82d631dec9430ecca9c13d48
basit438 <basitmanzoor438@gmail.com>
2025-09-16 10:33:57 +0530

    changed address format

--- src/app/contact-us/_components/ContactInfo.tsx
+++ src/app/contact-us/_components/ContactInfo.tsx
@@ -4,17 +4,27 @@ const ContactInfo = () => {
   return (
     <>
       <div className="bg-[#007BA8] rounded-[20px] lg:rounded-l-[20px] lg:rounded-r-[0] space-y-[30px] py-[30px] md:py-[60px] lg:py-[90px] px-[20px] md:px-[50px]">
-        <div className="border  bg-[#F2F2F8] rounded-[20px] px-[30px] py-[35px]">
-          <h3 className="text-[18px] md:text-[22px] font-semibold text-black mb-[20px] leading-none">
-            Office Address
-          </h3>
-          <div className="flex items-center text-black space-x-[15px] rtl:space-x-reverse">
-            <div className="shrink-0 bg-blue-200 text-[#007BA8] w-[48px] h-[48px] leading-[50px] text-[25px] inline-block text-center rounded-full">
-              <i className="bx bxs-map"></i>
-            </div>
-              <p>Headquarters - 30 N GOULD STREET, Suite R, Sheridan, WY 82801<br /> <br />Regional Office - 555 Republic Drive, Suite 450, Plano TX 75074<br /></p>
-          </div>
-        </div>
+        <div className="border bg-[#F2F2F8] rounded-[20px] px-[30px] py-[35px]">
+  <h3 className="text-[18px] md:text-[22px] font-semibold text-black mb-[20px] leading-none">
+    Office Address
+  </h3>
+  <div className="flex items-center text-black space-x-[15px] rtl:space-x-reverse">
+    <div className="shrink-0 bg-blue-200 text-[#007BA8] w-[48px] h-[48px] leading-[50px] text-[25px] inline-block text-center rounded-full">
+      <i className="bx bxs-map"></i>
+    </div>
+    <p>
+      <strong>Headquarters:</strong> <br />
+      30 N Gould Street <br />
+      Suite R <br />
+      Sheridan, TX 82801 <br /> <br />
+      <strong>Regional Office:</strong> <br />
+      555 Republic Drive <br />
+      Suite 450 <br />
+      Plano, TX 75074
+    </p>
+  </div>
+</div>
+
 
         {/*<div className="border border-[#585858] rounded-[20px] px-[30px] py-[35px]">
           <h3 className="text-[18px] md:text-[22px] font-semibold text-white mb-[20px] leading-none">
